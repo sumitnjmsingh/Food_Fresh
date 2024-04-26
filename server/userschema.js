ea53b8239ce3f673748c08c698d941455641684d
sumitnjmsingh <sumitsamsingh1111@gmail.com>
2024-04-27 02:10:29 +0530

    first commit

---
+++ server/userschema.js
@@ -0,0 +1,49 @@
+var mongoose = require('mongoose');
+
+
+const uri = 'mongodb+srv://sumitsamsingh1111:oTmGKMpL7XENaWnz@cluster0.pcwfqgz.mongodb.net/FOOD_FRENZY?retryWrites=true&w=majority&appName=Cluster0';
+
+
+mongoose.connect(uri)
+.then(() => {
+  console.log('Connected to MongoDB Atlas');
+})
+.catch((error) => {
+  console.error('Error connecting to MongoDB Atlas:', error);
+});
+
+
+
+
+
+
+
+
+
+// mongoose.connect("mongodb://127.0.0.1:27017/react1");
+// mongoose.connect("mongodb+srv://sumitsamsingh1111:oTmGKMpL7XENaWnz@cluster0.pcwfqgz.mongodb.net/react1");
+// mongoose.connect("mongodb+srv://sumitsamsingh1111:wqrT8hRMpX2U7eQi@cluster0.xz423hp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+const userSchema = new mongoose.Schema({
+name:{
+    type:String,
+    required:true
+},
+email:{
+    type:String,
+    required:true
+},
+password:{
+    type:String,
+    required:true
+},
+number:{
+    type:String,
+    required:true
+},
+  
+});
+
+
+const User = mongoose.model('User', userSchema);
+ 
+module.exports = User;
